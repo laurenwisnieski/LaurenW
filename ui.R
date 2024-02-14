a3b4cfcf83453a29f33219b14f3eaa570236e442
laurenwisnieski <lauren.wisnieski@lmunet.edu>
2024-02-14 11:37:59 +0000

    Practice

---
+++ ui.R
@@ -0,0 +1,37 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+fluidPage(
+  
+  # Application title
+  titlePanel("Ye Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    position = c("right") , 
+    sidebarPanel(
+      sliderInput("bins",
+                  "Bins (#):",
+                  min = 1,
+                  max = 70,
+                  width = "150%",
+                  value = 30) , 
+    ) ,
+    
+    
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+      
+    ))
+)
