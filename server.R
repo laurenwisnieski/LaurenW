734f1e2fe61caa8365d1fac8ee31dc13c1e0fd6e
laurenwisnieski <lauren.wisnieski@lmunet.edu>
2024-02-14 23:29:36 +0000

    Challenge 2

--- server.R
+++ server.R
@@ -1,31 +1,23 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define server logic required to draw a histogram
-function(input, output, session) {
+server <- function(input, output) {
   
   output$distPlot <- renderPlot({
-    
     # generate bins based on input$bins from ui.R
     x    <- faithful[, 2]
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'purple', border = 'white',
-         xlab = 'Minutes of waiting time',
-         main = 'Waiting times', 
-         ylab= "Density")
-    
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
   })
   
+  output$distPlot2 <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'pink', border = 'white')
+  })
 }
-
-
--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
 #
 # Find out more about building applications with Shiny here:
 #
@@ -10,28 +10,46 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-fluidPage(
+ui <- fluidPage(
   
   # Application title
-  titlePanel("Ye Very very Old Faithful Geyser Data"),
+  fluidRow(
+    column(3, 
+           titlePanel("Old Faithful Geyser Data")) , 
+    tags$img(src = "https://th.bing.com/th/id/OIP.x2s6ljznyAeATppifNstuwHaLH?w=126&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7")
+  ) , 
   
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    position = c("right") , 
-    sidebarPanel(
-      sliderInput("bins",
-                  "Bins (#):",
-                  min = 1,
-                  max = 70,
-                  width = "75%",
-                  value = 30) , 
-    ) ,
-    
-    
+  
+  # Sidebar with a slider input for number of bins 
+  fluidRow(
+    column(3, 
+           wellPanel(
+             sliderInput("bins",
+                         "Number of bins:",
+                         min = 1,
+                         max = 50,
+                         value = 30))
+    ),
     
     # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-      
-    ))
+    column(9, 
+           tabsetPanel(
+             tabPanel("Plot",
+                      plotOutput("distPlot")
+             ),
+             tabPanel("Plot2",
+                      plotOutput("distPlot2")
+             ))
+    )
+  ), 
+  
+  fluidRow(
+    column(4, 
+           tags$footer("Lauren Wisnieski")) , 
+    column(4, 
+           tags$a(href= "mailto:lauren.wisnieski@lmunet.edu", "Email me" )) , 
+    column(4, 
+           tags$a(href= "https://www.linkedin.com/in/lauren-wisnieski", "LinkedIn Profile",  target="_blank")))
+  
+  
 )
